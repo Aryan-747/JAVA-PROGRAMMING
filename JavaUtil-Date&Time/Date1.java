de2e12d269849a1db43e9f79197382a70bbadb2f
Aryan Salaria <arayn.32@gmail.com>
2024-03-29 13:25:40 +0530

    Date 1

---
+++ JavaUtil-Date&Time/Date1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Date1
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        Date d1 = new Date();
+
+        System.out.println(d1);
+    }
+
+
+}
