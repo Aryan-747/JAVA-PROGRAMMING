8b96950e72089ee5b796d6f2e8e303f5bce1bcae
Aryan Salaria <arayn.32@gmail.com>
2024-04-20 21:02:19 +0530

    Basic Inheritance Question 2

---
+++ PracticeQues/Inheritance2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+// AryanxD
+
+class Product
+{
+	String prodname;
+	double Price;
+}
+
+class ProductInfo extends Product
+{
+	void inputProductDetails()
+	{
+		Scanner myscanner = new Scanner(System.in);
+		
+		System.out.print("product name: ");
+		prodname = myscanner.nextLine();
+		System.out.print("price: $");
+		Price = myscanner.nextDouble();
+	}
+	
+	void displayProductInfo()
+	{
+		System.out.println("product name: " + prodname);
+		System.out.println("price: $" + Price);
+	}
+}
+
+
+
+
+public class Inheritance2
+{
+    public static void main(String[] args) {
+        ProductInfo productData = new ProductInfo();
+        productData.inputProductDetails();
+        productData.displayProductInfo();
+    }
+}
