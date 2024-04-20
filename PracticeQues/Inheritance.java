3a7ef2053a7ae1185684ae192921c804dd4864fe
Aryan Salaria <arayn.32@gmail.com>
2024-04-20 21:00:24 +0530

    Inheritance 1

---
+++ PracticeQues/Inheritance.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+	
+class NumberIn
+{
+	int num;
+	
+	void inputNum()
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		num = scan.nextInt();
+	}
+}
+
+class SquareOut extends NumberIn
+{
+	void displaySquare()
+	{
+		System.out.println(num*num);
+	}
+}
+	
+
+public class Inheritance {
+    public static void main(String[] args) {
+    	SquareOut squareout = new SquareOut();
+        squareout.inputNum();
+        squareout.displaySquare();
+    }
+}
