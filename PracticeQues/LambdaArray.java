c8a52027f551f6de4565a64aec527ef124c65dcf
Aryan Salaria <arayn.32@gmail.com>
2024-04-21 22:16:50 +0530

    Lambda Function with array as parameter

---
+++ PracticeQues/LambdaArray.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Step 1: Create a functional interface
+interface MaxValueFinder {
+    int findMax(int[] array);
+}
+
+public class LambdaArray {
+    public static void main(String[] args) {
+
+	Scanner newscan = new Scanner(System.in);
+	
+	int size;
+	int k;
+	
+	System.out.print("Size of the array: ");
+	size = newscan.nextInt();
+	
+	int[] array = new int[size];
+	
+	for(int i=0 ; i<size ; i++)
+	{
+		System.out.print("Element " + (i+1) + ": ");
+		array[i] = newscan.nextInt();
+	}
+	
+	Arrays.sort(array);
+	
+	System.out.println(array[array.length-1]);
+	
+	
+	
+
+}
+}
