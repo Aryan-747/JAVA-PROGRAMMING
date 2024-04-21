b360652ce7c7f1a47629444bf94d81028faf1b2b
Aryan Salaria <arayn.32@gmail.com>
2024-04-21 22:22:10 +0530

    Word Capitalizer

---
+++ PracticeQues/WordCapitalizer.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.*;
+
+// This Program capitalizes the first letter of every word in the given input sentence
+
+
+class WordCapitalizer
+{
+	String word;
+	
+	WordCapitalizer(String para)
+	{
+		this.word = para;	
+	}
+	
+	class CapitalizationProcessor
+	{
+		char[] wordarray = word.toCharArray();
+		char[] capitalizeWords()
+		{
+			wordarray[0] = Character.toUpperCase(wordarray[0]);
+			for(int i=1 ; i<word.length() ; i++)
+			{
+				if(wordarray[i] == ' ')
+				{
+					wordarray[i+1] = Character.toUpperCase(wordarray[i+1]);
+				}
+			}
+			
+			return wordarray;
+		}
+	}
+
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.nextLine();
+
+        WordCapitalizer wordCapitalizer = new WordCapitalizer(inputString);
+
+        CapitalizationProcessor capitalizationProcessor = wordCapitalizer.new CapitalizationProcessor();
+
+        System.out.println(capitalizationProcessor.capitalizeWords());
+
+        scanner.close();
+    }
+}
