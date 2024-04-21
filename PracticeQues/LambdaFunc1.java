b3d7dd1a5e6cd4c840616b72b992492a53d6395b
Aryan Salaria <arayn.32@gmail.com>
2024-04-21 22:15:41 +0530

    Factorial of Number using Lambda Function

---
+++ PracticeQues/LambdaFunc1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+@FunctionalInterface
+interface FactorialCalculator {
+    long calculateFactorial(int number);
+}
+
+public class LambdaFunc1{
+    public static void main(String[] args) {
+
+	Scanner scan = new Scanner(System.in);
+	
+	int num;
+	
+	num = scan.nextInt();
+	
+	FactorialCalculator factorial = number -> 
+	{
+		int k=1;
+		
+		for(int i=1 ; i<=number ; i++)
+		{
+			k *=i;
+			
+		}
+		
+		return k;
+	};
+	
+	System.out.println("Factorial: " + factorial.calculateFactorial(num));
+	
+}
+}
